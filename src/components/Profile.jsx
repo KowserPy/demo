f9cb848aeeffac8b14cef80bc079660b3d3a59d8
Kowser <107254329+KowserPy@users.noreply.github.com>
2024-09-27 15:23:56 +0600

    first commit 2

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -15,14 +15,21 @@ const Profile = () => {
 		<div className="bg-gradient-to-r from-blue-200 to-cyan-200 p-5 rounded-lg shadow-lg w-full max-w-md">
 			<div className="flex flex-col items-center mb-6">
 				<img src={woofImg} alt="woofImg" className="w-2/5" />
-				<span className="text-3xl font-bold">7,809 WOOF</span>
+				<span className="text-3xl font-bold">{profile.points.totalPoints}</span>
 			</div>
 			<div className="space-y-4">
 				<div className="flex items-center justify-between bg-gray-50 p-4 rounded-lg shadow-sm">
-					<span className="font-semibold">Points</span>
+					<span className="font-semibold">Task</span>
 					<div className="flex items-center gap-2">
-						<span>{profile.points.totalPoints}</span>
-						<img src={woofImg} alt="Reward Icon" className="w-6 h-6" />
+						<span>{profile.points.taskPoints}</span>
+						<img src={woofImg} alt="Task Icon" className="w-6 h-6" />
+					</div>
+				</div>
+				<div className="flex items-center justify-between bg-gray-50 p-4 rounded-lg shadow-sm">
+					<span className="font-semibold">Invites</span>
+					<div className="flex items-center gap-2">
+						<span>{profile.points.referralPoints}</span>
+						<img src={woofImg} alt="Invite Icon" className="w-6 h-6" />
 					</div>
 				</div>
 			</div>
