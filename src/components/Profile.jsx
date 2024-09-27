cf852d18d6321c4344e89f6f39f270337f925bfb
Kowser <107254329+KowserPy@users.noreply.github.com>
2024-09-27 15:47:09 +0600

    update profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -28,20 +28,20 @@ const Profile = () => {
 		<div className="bg-gradient-to-r from-blue-200 to-cyan-200 p-5 rounded-lg shadow-lg w-full max-w-md">
 			<div className="flex flex-col items-center mb-6">
 				<img src={woofImg} alt="woofImg" className="w-2/5" />
-				<span className="text-3xl font-bold">{totalPoints}WOOF</span>
+				<span className="text-3xl font-bold">{totalPoints} WOOF</span>
 			</div>
 			<div className="space-y-4">
 				<div className="flex items-center justify-between bg-gray-50 p-4 rounded-lg shadow-sm">
 					<span className="font-semibold">Task</span>
 					<div className="flex items-center gap-2">
-						<span>{taskPoints}WOOF</span>
+						<span>{taskPoints} WOOF</span>
 						<img src={woofImg} alt="Task Icon" className="w-6 h-6" />
 					</div>
 				</div>
 				<div className="flex items-center justify-between bg-gray-50 p-4 rounded-lg shadow-sm">
 					<span className="font-semibold">Invites</span>
 					<div className="flex items-center gap-2">
-						<span>{referralPoints}WOOF</span>
+						<span>{referralPoints} WOOF</span>
 						<img src={woofImg} alt="Invite Icon" className="w-6 h-6" />
 					</div>
 				</div>
