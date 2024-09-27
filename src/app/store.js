bf000552046acc508cf07c00c15cf1d61e312fb3
Kowser <107254329+KowserPy@users.noreply.github.com>
2024-09-27 15:11:43 +0600

    first commit 2

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import taskReducer from "../features/task/TaskSlice";
+import userReducer from "../features/user/userSlice";
 
 const store = configureStore({
 	reducer: {
 		auth: authReducer,
-		tasks: taskReducer,
+		user: userReducer,
 	},
 });
 
