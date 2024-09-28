67826846cc8ecc3dbf878ba633a9bbfab813cd4e
Kowser <107254329+KowserPy@users.noreply.github.com>
2024-09-28 12:33:50 +0600

    update profile 2

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,16 @@ createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
 			<RouterProvider router={router} />
-			<ToastContainer />
+			<ToastContainer
+				position="top-right" // You can change position (top-left, bottom-right, etc.)
+				autoClose={5000} // Set auto close in milliseconds
+				hideProgressBar={false} // Show or hide progress bar
+				newestOnTop={false} // Show the newest toast on top
+				closeOnClick // Close toast when user clicks
+				pauseOnHover // Pause toast timer when hovered
+				draggable // Allow dragging the toast
+				theme="colored" // Optional: "colored", "light", or "dark"
+			/>
 		</Provider>
 	</StrictMode>
 );
